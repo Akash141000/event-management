fcaba731005e5a23a8df06f6285202040e4cd595
akash padwal <akashpadwal21@gmail.com>
2022-03-19 13:26:45 +0530

    role assignment added

--- terraform/modules/cluster/cluster.tf
+++ terraform/modules/cluster/cluster.tf
@@ -25,6 +25,7 @@ resource "azurerm_kubernetes_cluster" "az_kubernetes_cluster" {
       client_secret = var.serviceprinciple_key
     }
 
+  
     default_node_pool {
       name = "default"
       node_count = 1
@@ -68,5 +69,11 @@ resource "azurerm_kubernetes_cluster" "az_kubernetes_cluster" {
 
     }
    
+}
 
+# add the role to the identity the kubernetes cluster was assigned
+resource "azurerm_role_assignment" "AcrPull" {
+  scope                = azurerm_container_registry.az_container_registry.id
+  role_definition_name = "AcrPull"
+  principal_id         = var.serviceprinciple_id
 }
